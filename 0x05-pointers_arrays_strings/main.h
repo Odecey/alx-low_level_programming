cefcf0660bd9ac8da643eba535c7ce014a717417
onyeka daniel <odecey52@gmail.com>
2022-10-25 06:14:21 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-void reset_to_98((int *n);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t_strlen(const char *str);
 void _puts(char *str);
