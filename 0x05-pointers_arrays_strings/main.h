360a6a5e783f2de5dfc63dea88ea021fd9258919
onyeka daniel <odecey52@gmail.com>
2022-10-26 05:30:01 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int size_t_strlen(const char *str);
+size_t_strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void puts2(char *str);
