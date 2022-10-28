533122cbdc6d9a09a66dfa7cc1eaa62e36f87add
onyeka daniel <odecey52@gmail.com>
2022-10-28 10:11:32 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,4 @@
-##include "main.h"
+#include "main.h"
 #include <stdio.h>
 /**
  * infinite_add - adds two numbers
@@ -43,7 +43,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			c2--, dr2 = *(n2 + c2) - 48;
 		else
 			dr2 = 0;
-		bg--, size_r--,
+		bg--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
