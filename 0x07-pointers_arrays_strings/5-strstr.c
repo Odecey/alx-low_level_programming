15a51854af99b4a5041e5a5ff5affb73ba4f8262
onyeka daniel <odecey52@gmail.com>
2022-11-01 09:36:13 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strstr - locate a substring
+ * @haystack: the string to search
+ * @needle: the string to find 
+ *
+ * Return: char value
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0, b = 0;
+
+	while
