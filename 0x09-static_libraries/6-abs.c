6b17900d7491d22b14471ac5109138585e28a8f2
onyeka daniel <odecey52@gmail.com>
2022-11-05 09:03:35 -0700

    _putchar.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - print the absolutevalue of an integer.
+ * @a: the integer to compute its absolute value.
+ * Return: returns the abs value.
+ *
+ */
+int _abs(int a)
+{
+	int k = a
+	int absvalue;
+
+	if (k < 0)
+	{
+		k = k * (-1);
+	}
+	absvalue = k;
+	return (absvalue);
+}
