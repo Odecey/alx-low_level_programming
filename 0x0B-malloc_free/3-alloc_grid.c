fb5068f23bac1b1c9630c4d1699db3c43402fb3f
onyeka daniel <odecey52@gmail.com>
2022-11-08 20:54:04 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		array[i] = (int *) malloc(sizeof(int) * width);
 
 		if
-			(array[i] == NULL);
+			(array[i] == NULL)
 		{
 			free(array);
 			for (j = 0; j <= i; j++)
