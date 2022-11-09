aae7791dd62832bc5c735a841be0c68fb2760988
onyeka daniel <odecey52@gmail.com>
2022-11-08 20:40:33 -0800

    100-argstostr.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -18,7 +18,7 @@ int count_word(char *s)
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (s[c] == '0')
+		if (s[c] == "")
 			flag = 0;
 		else if (flag == 0)
 		{
@@ -56,7 +56,7 @@ char **strtow(char *str)
 
 	for (i = 0; i <= len; i++)
 	{
-		if (str[i] == '0' || str[i] == '\0')
+		if (str[i] == "" || str[i] == '\0')
 		{
 			if (c)
 			{
